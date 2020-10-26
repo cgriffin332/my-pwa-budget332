d9d271fa0e49f8feb38819e2275c2edcb47f370c
Calvin Griffin <cgriffin332@gmail.com>
2020-10-26 19:04:32 -0400

    success and error

--- public/db.js
+++ public/db.js
@@ -8,6 +8,31 @@ request.onupgradeneeded = function(event) {
   db.createObjectStore("pending", { autoIncrement: true });
 };
 
+request.onsuccess = function(event) {
+    db = event.target.result;
+  
+    // make sure app is online before reading from db
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
+  
+  request.onerror = function(event) {
+    console.log("Woops! " + event.target.errorCode);
+  };
+  
+  function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+  
+    // access pending object store
+    const store = transaction.objectStore("pending");
+  
+    // add record to store with add method.
+    store.add(record);
+  }
+  
+  
 
 
 // listen for app coming back online
